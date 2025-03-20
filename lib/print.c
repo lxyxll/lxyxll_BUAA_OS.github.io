cd5382b0eb6f3b76d1619f84d803447ee6ecdeec
黎欣妍 <23371341@buaa.edu.cn>
2025-03-20 22:26:34 +0800

    lab1

--- lib/print.c
+++ lib/print.c
@@ -19,25 +19,47 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 	for (;;) {
 		/* scan for the next '%' */
 		/* Exercise 1.4: Your code here. (1/8) */
-
+                while(*fmt!='%'&&*fmt!='\0')
+		{
 		/* flush the string found so far */
-		/* Exercise 1.4: Your code here. (2/8) */
-
+ 		/* Exercise 1.4: Your code here. (2/8) */
+                 out(data,fmt,1);
+		 fmt++;
+		}
 		/* check "are we hitting the end?" */
 		/* Exercise 1.4: Your code here. (3/8) */
-
+                if (*fmt == '\0')
+		{break;}
 		/* we found a '%' */
 		/* Exercise 1.4: Your code here. (4/8) */
-
+                fmt++;   //skip %
 		/* check format flag */
 		/* Exercise 1.4: Your code here. (5/8) */
+               padc = ' ';    // 默认填充空格
+               ladjust = 0;    // 默认右对齐
+               if (*fmt == '-') {
+               ladjust = 1;
+               fmt++;
+               } else if (*fmt == '0') {
+               padc = '0'; // 填充 '0'
+               fmt++;
+               }
 
 		/* get width */
 		/* Exercise 1.4: Your code here. (6/8) */
-
+               width = 0;
+	       while(*fmt >= '0' && *fmt <= '9')
+	       {
+		       width = width*10 + *fmt-'0';
+		       fmt++;
+	       }
 		/* check for long */
 		/* Exercise 1.4: Your code here. (7/8) */
-
+               long_flag = 0;
+	       if (*fmt == 'l')
+	       {long_flag = 1;
+	       fmt++;
+	       }//
 		neg_flag = 0;
 		switch (*fmt) {
 		case 'b':
@@ -63,7 +85,12 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 			 * others. (hint: 'neg_flag').
 			 */
 			/* Exercise 1.4: Your code here. (8/8) */
-
+                        if (num < 0)
+			{
+				neg_flag = 1;
+				num = -num; 
+			}
+			print_num(out, data, num, 10, neg_flag, width, ladjust, padc, 0);
 			break;
 
 		case 'o':
@@ -85,47 +112,45 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 			}
 			print_num(out, data, num, 10, 0, width, ladjust, padc, 0);
 			break;
-
-		case 'x':
-			if (long_flag) {
-				num = va_arg(ap, long int);
-			} else {
-				num = va_arg(ap, int);
-			}
-			print_num(out, data, num, 16, 0, width, ladjust, padc, 0);
-			break;
-
-		case 'X':
-			if (long_flag) {
-				num = va_arg(ap, long int);
-			} else {
-				num = va_arg(ap, int);
-			}
-			print_num(out, data, num, 16, 0, width, ladjust, padc, 1);
-			break;
-
-		case 'c':
-			c = (char)va_arg(ap, int);
-			print_char(out, data, c, width, ladjust);
-			break;
-
-		case 's':
-			s = (char *)va_arg(ap, char *);
-			print_str(out, data, s, width, ladjust);
-			break;
-
-		case '\0':
-			fmt--;
-			break;
-
-		default:
-			/* output this char as it is */
-			out(data, fmt, 1);
-		}
-		fmt++;
-	}
-}
-
+                case 'x':
+                          if (long_flag) {
+                                  num = va_arg(ap, long int);
+                          } else {
+                                  num = va_arg(ap, int);
+                          }
+                          print_num(out, data, num, 16, 0, width, ladjust, padc, 0);
+                          break;
+  
+                  case 'X':
+                          if (long_flag) {
+                                 num = va_arg(ap, long int);
+                         } else {
+                                 num = va_arg(ap, int);
+                         }
+                         print_num(out, data, num, 16, 0, width, ladjust, padc, 1);
+                         break;
+ 
+                 case 'c':
+                         c = (char)va_arg(ap, int);
+                         print_char(out, data, c, width, ladjust);
+                         break;
+ 
+                 case 's':
+                         s = (char *)va_arg(ap, char *);
+                         print_str(out, data, s, width, ladjust);
+                         break;
+ 
+                 case '\0':
+                         fmt--;
+                         break;
+ 
+                 default:
+                         /* output this char as it is */
+                         out(data, fmt, 1);
+                 }
+                 fmt++;
+        }
+ }
 /* --------------- local help functions --------------------- */
 void print_char(fmt_callback_t out, void *data, char c, int length, int ladjust) {
 	int i;
